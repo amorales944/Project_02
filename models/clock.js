680937d91c1d2b842981554b8811803f62b3263e
unknown <pwhynotfilms@gmail.com>
2019-06-19 20:43:42 -0500

    link fixed on sign_in.html

--- models/clock.js
+++ models/clock.js
@@ -1,8 +1,9 @@
 module.exports = function(sequelize, Datatypes) {
     var Clock = sequelize.define("Clock", {
         name: Datatypes.STRING,
-        clockIn: Datatypes.TIMESTAMP,
-        clockOut: Datatypes.TIMESTAMP
+        in: Datatypes.TIMESTAMP,
+        out: Datatypes.TIMESTAMP,
+        lunch: Datatypes.TIMESTAMP
     });
 
     
