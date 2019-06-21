838bb65448cd716f4baffe799bb3a3c10a43ffc6
unknown <pwhynotfilms@gmail.com>
2019-06-20 21:17:34 -0500

    seeds.sql

---
+++ seeds.sql
@@ -0,0 +1,5 @@
+USE work_schedule;
+INSERT INTO schedules (employee, work_date, start_hour, end_hour)
+VALUES 
+("Patrick", 07/01/2019, "08:00:00", "06:00:00"),
+("Colton", 07/02/2019, "06:30:00", "05:00:00");
